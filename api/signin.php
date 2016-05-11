63364caabf07c9c3dd63da67700d01aead9100f4
lactozily <lactoz.ily@gmail.com>
2016-05-12 00:03:45 +0700

    step-2

--- api/signin.php
+++ api/signin.php
@@ -2,4 +2,10 @@
   if ($_SERVER["REQUEST_METHOD"] != "POST") {
     header("Location: /");
   }
+
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  echo $username;
+  echo $password;
 ?>
